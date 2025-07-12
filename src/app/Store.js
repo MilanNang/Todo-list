3ac0ae0a4f41fb095bdec617fa60fdaf975e37bf
MilanNang <milanrayka03@gmail.com>
2025-07-12 13:55:46 +0530

    add Prodect

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,12 +1,3 @@
-/*import { configureStore } from "@reduxjs/toolkit";
-import { todoReducer } from "../Fetures/Todo/todoSlice";
-
-
-
-export const store=configureStore({
-    reducer : todoReducer
-}
-)*/
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../Fetures/Todo/todoSlice"; // Default import
 
